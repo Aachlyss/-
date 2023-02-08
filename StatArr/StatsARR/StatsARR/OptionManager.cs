d88f18cffc677d0f02f64c47e7ff361982b3c7bf
Aachlyss <ivanangelov05@gmail.com>
2023-02-08 09:13:57 +0200

    Second Commit - Odd Sum and Odd nums

--- StatArr/StatsARR/StatsARR/OptionManager.cs
+++ StatArr/StatsARR/StatsARR/OptionManager.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace StatsARR
 {
-    class OptionManager
+    public static class OptionManager
     {
+        public static int CountLess100(int[]arr)
+        {
+            return arr.Count(x => x < 100);
+        }
+        public static int Max(int[] arr)
+        {
+            return arr.Max();
+        }
+        public static int[] ReverseArr(int[] arr)
+        {
+            return arr.Reverse().ToArray();
+        }
+        public static void OddNums(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(", ",result));
+        }
+        public static void OddSum(int[] arr)
+        {
+            var result = arr.Where(x => x % 2 == 0).Sum();
+            Console.WriteLine(string.Join(", ", result));
+        }
+        public static void PrintArr(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ",arr));    
+        }
+
     }
 }
--- StatArr/StatsARR/StatsARR/Program.cs
+++ StatArr/StatsARR/StatsARR/Program.cs
@@ -6,7 +6,19 @@ namespace StatsARR
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(OptionManager.CountLess100(arr));
+            Console.WriteLine(OptionManager.Max(arr));
+            arr = OptionManager.ReverseArr(arr);
+            OptionManager.PrintArr(arr);
+            OptionManager.OddNums(arr);
+            OptionManager.OddSum(arr);
+            //
         }
     }
 }
