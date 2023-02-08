c9c04cae07f1f655d79140fa11823acb09446077
Aachlyss <ivanangelov05@gmail.com>
2023-02-08 08:59:18 +0200

    First Commit

---
+++ StatArr/StatsARR/StatsARR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatsARR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
