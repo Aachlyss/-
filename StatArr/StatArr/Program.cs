dff70025b913b693c093f2655044785af65862bb
Aachlyss <ivanangelov05@gmail.com>
2023-02-08 08:45:57 +0200

    First Commit

---
+++ StatArr/StatArr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StatArr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(OptionManager.CountLess100(arr));
+            Console.WriteLine(OptionManager.MaxNum(arr));
+            arr = OptionManager.Reverce(arr);
+            OptionManager.PrintArr(arr);
+            //
+        }
+    }
+}
