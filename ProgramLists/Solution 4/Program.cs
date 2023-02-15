c1b381ca872d5927a71d7a196886822e323cd810
Aachlyss <ivanangelov05@gmail.com>
2023-02-15 09:42:10 +0200

    Commit Solution 4

--- ProgramLists/Solution 4/Program.cs
+++ ProgramLists/Solution 4/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Solution_4
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter a list of integers separated by spaces: ");
+        string input = Console.ReadLine();
+        int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+        List<int> squares = new List<int>();
+        foreach (int number in numbers)
         {
-            Console.WriteLine("Hello World!");
+            if (IsSquare(number))
+            {
+                squares.Add(number);
+            }
         }
+
+        squares.Sort();
+        squares.Reverse();
+
+        Console.WriteLine("Number squares in decreasing order: " + string.Join(" ", squares));
+    }
+
+    static bool IsSquare(int number)
+    {
+        int root = (int)Math.Sqrt(number);
+        return root * root == number;
     }
 }
