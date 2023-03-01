807655254466fab83f3adebe890206889f3949b0
Aachlyss <ivanangelov05@gmail.com>
2023-03-01 08:47:55 +0200

    LastCommitReworked

--- ProgramLists/Solution 5/Program.cs
+++ ProgramLists/Solution 5/Program.cs
@@ -21,14 +21,19 @@ class MainClass
             else if (command[0] == "addMany")
             {
                 int index = int.Parse(command[1]);
-                List<int> elements = command.Skip(2).Select(int.Parse).ToList();
-                numbers.InsertRange(index, elements);
+                //List<int> elements = command.Skip(2).Select(int.Parse).ToList();
+                //numbers.InsertRange(index, elements);
+                for (int i = command.Count()-1; i >= 2; i--)
+                {
+                    numbers.Insert(index,int.Parse(command[i]));
+                }
             }
             else if (command[0] == "contains")
             {
                 int element = int.Parse(command[1]);
                 int index = numbers.IndexOf(element);
                 Console.WriteLine(index);
+                
             }
             else if (command[0] == "remove")
             {
@@ -38,17 +43,21 @@ class MainClass
             else if (command[0] == "shift")
             {
                 int positions = int.Parse(command[1]);
-                for (int i = 0; i < positions; i++)
+                
+                for (int i = 0; i < positions%numbers.Count; i++)
                 {
                     int firstElement = numbers[0];
                     numbers.RemoveAt(0);
                     numbers.Add(firstElement);
+
+
                 }
+                
             }
             else if (command[0] == "sumPairs")
             {
                 List<int> result = new List<int>();
-                for (int i = 0; i < numbers.Count - 1; i += 2)
+                for (int i = 0; i < numbers.Count ; i += 2)
                 {
                     result.Add(numbers[i] + numbers[i + 1]);
                 }
