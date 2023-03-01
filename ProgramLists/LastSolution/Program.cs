1937b89bdc366daf4fc7cccaa9ea8be1ea6e3683
Aachlyss <ivanangelov05@gmail.com>
2023-03-01 09:23:18 +0200

    Commit for last ex

---
+++ ProgramLists/LastSolution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LastSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+                if (command[0]=="end")
+                {
+                    break;
+                }
+                switch (command[0])
+                {
+                    case "Contains":
+                        int numberu = Convert.ToInt32(command[1]);
+                        if (numbers.Contains(numberu))
+                        {
+                            Console.WriteLine("Yes");
+                        }
+                        if (numbers!.Contains(numberu))
+                        {
+                            Console.WriteLine("No such nummber!");
+                        }
+                        break;
+                    case "PrintEven":
+                            Console.WriteLine(string.Join(' ',numbers.Where(x=>x%2==0)));
+                        break;
+                    case "Filter":
+                        break;
+                }
+            }
+        }
+    }
+}
